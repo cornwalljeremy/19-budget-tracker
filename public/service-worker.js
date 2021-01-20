cce68371f1aaf4fe59872ef3f84f5c9ce1bd0cbf
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-20 14:57:00 -0700

    more

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,29 @@
-const CACHE_NAME = 'budgettracker-site-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = "budget_tracker";
+const DATA_CACHE_NAME = "data-cache_v1";
 
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/css/styles.css',
-  '/js/index.js',
-  '/js/idb.js',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
-  '/api/transaction'];
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/css/styles.css",
+  "/js/index.js",
+  "/js/idb.js",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/api/transaction",
+];
 
-self.addEventListener('install', function (evt) {
+self.addEventListener("install", function (evt) {
   evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!');
-      return cache.addAll(FILES_TO_CACHE)
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("You pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 
@@ -31,13 +32,13 @@ self.addEventListener('install', function (evt) {
 //
 
 // Activate the service worker and remove old data from the cache
-self.addEventListener('activate', function (evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
-    caches.keys().then(keyList => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
+            console.log("Get it gone!", key);
             return caches.delete(key);
           }
         })
@@ -50,27 +51,25 @@ self.addEventListener('activate', function (evt) {
 //
 
 // Intercept fetch requests
-self.addEventListener('fetch', function (evt) {
-  if (evt.request.url.includes('/api/')) {
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
-        .then(cache => {
+        .then((cache) => {
           return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
+            .then((response) => {
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
 
               return response;
             })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
+            .catch((err) => {
               return cache.match(evt.request);
             });
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err))
     );
 
     return;
@@ -81,9 +80,8 @@ self.addEventListener('fetch', function (evt) {
       return caches.match(evt.request).then(function (response) {
         if (response) {
           return response;
-        } else if (evt.request.headers.get('accept').includes('text/html')) {
-          // return the cached home page for all requests for html pages
-          return caches.match('/');
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
         }
       });
     })
